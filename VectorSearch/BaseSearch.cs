a8076bf2a35f0235defc3ddccfcd0416d1f1e4ad
Michael Gallinago <michael.gallinago@gmail.com>
2023-12-10 16:19:47 +0500

    BaseSearch

---
+++ VectorSearch/BaseSearch.cs
@@ -0,0 +1,17 @@
+﻿namespace VectorSearch;
+
+public class BaseSearch
+{
+    public static int Search(IReadOnlyList<double> target, List<IReadOnlyList<double>> vectors)
+    {
+        for (var i = 0; i < vectors.Count; i++)
+        {
+            if (VectorUtilities.GetDifferenceSign(target, vectors[i]) == 0)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
